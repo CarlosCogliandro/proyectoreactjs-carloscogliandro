7d2c74e54c8b70822232df10c1d2a9ad67cbc2fd
Carlos Cogliandro <carloscogliandro22@gmail.com>
2022-10-06 00:17:37 +0200

    Borrando comentarios, puliendo proyecto para la entrega final

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,7 +2,6 @@ import './cart-styles.css'
 import { useContext, useState } from "react"
 import { CartContext } from "../../context/CartContext"
 import { Link, useNavigate } from "react-router-dom"
-// import ItemCount from '../ItemCount/ItemCount'
 import { collection, addDoc, getFirestore, updateDoc, doc } from 'firebase/firestore'
 import moment from 'moment'
 
@@ -10,73 +9,6 @@ import moment from 'moment'
 const Cart = () => {
 
   const { cart, removeItem, clear, totalQuantity } = useContext(CartContext);
-
-
-  // // Firebase ----------------------------------------------
-
-  // const navigate = useNavigate();
-
-  // const [order, setOrder] = useState({
-  //   buyer: {
-  //     name: '',
-  //     phone: 0,
-  //     email: '',
-  //   },
-  //   item: [],
-  //   total: 0,
-  //   date: '',
-  // });
-
-  // const db = getFirestore();
-
-  // const createOrder = () => {
-  //   setOrder((currentOrder) => {
-  //     return {
-  //       ...currentOrder,
-  //       items: cart,
-  //       total: cart.reduce((acc, item) => acc + item.price * item.quantity, 0),
-  //       date: moment().format('DD/MM/YYYY, h:mm:ss a'),
-  //     }
-  //   });
-
-  //   const query = collection(db, 'orders');
-  //   addDoc(query, order)
-  //     .then(({ id }) => {
-  //       console.log(id)
-  //       updateStockProducts();
-  //       alert('Gracias por tu compra')
-  //     })
-  //     .catch(() =>
-  //       alert('Tu compra no fue realizada. Intenta mas tarde')
-  //     );
-  // };
-
-  // const updateStockProducts = () => {
-  //   cart.forEach((product) => {
-  //     const queryUpdate = doc(db, 'items', product.id);
-  //     updateDoc(queryUpdate, {
-  //       categoryId: product.categoryId,
-  //       parrafo: product.parrafo,
-  //       image: product.image,
-  //       price: product.price,
-  //       tittle: product.tittle,
-  //       stock: product.stock - product.quantity,
-  //     })
-  //       .then(() => {
-  //         if (cart[cart.length - 1].id === product.id) {
-  //           clear();
-  //           navigate('/');
-  //         }
-  //       })
-  //       .catch(() => {
-  //         console.log('Error al actualizar stock');
-  //       });
-  //   });
-  // };
-
-
-  // Firebase ----------------------------------------------
-
 
   return (
     <div className='cart'>
@@ -109,7 +41,7 @@ const Cart = () => {
 
             <button onClick={() => clear()} className='boton-agregarCart2'>Vaciar carrito</button>
 
-            <Link to={'/finalizarcompra'}><button /*onClick={createOrder}*/ className='boton-agregarCart'>Finalizar comprar</button></Link>
+            <Link to={'/finalizarcompra'}><button className='boton-agregarCart'>Finalizar comprar</button></Link>
 
           </div>
         </>
@@ -119,168 +51,3 @@ const Cart = () => {
 }
 
 export default Cart;
-
-
-
-
-
-// https://repo-ecommerce.vercel.app/
-
-
-
-
-
-
-
-// import { useContext, useState } from 'react';
-// import { CartContext } from '../../context/cartContext';
-// import { Link } from 'react-router-dom';
-// import moment from 'moment';
-// import { useNavigate } from 'react-router-dom';
-// import {
-//   collection,
-//   addDoc,
-//   getFirestore,
-//   updateDoc,
-//   doc,
-// } from 'firebase/firestore';
-
-// const Cart = () => {
-//   const navigate = useNavigate();
-//   const { cart, removeItem, clear } = useContext(CartContext);
-//   const [order, setOrder] = useState({
-//     buyer: {
-//       name: '',
-//       phone: 0,
-//       email: '',
-//     },
-//     items: cart,
-//     total: cart.reduce((acc, item) => acc + item.price * item.quantity, 0),
-//     date: moment().format('DD/MM/YYYY, h:mm:ss a'),
-//   });
-
-//   const db = getFirestore();
-
-//   const createOrder = () => {
-//     const query = collection(db, 'orders');
-//     addDoc(query, order)
-//       .then(({ id }) => {
-//         console.log(id);
-//         updateStockProducts();
-//         alert('Felicidades por tu compra');
-//       })
-//       .catch(() =>
-//         alert('Tu compra no pudo ser completada, intentalo más tarde')
-//       );
-//   };
-
-//   const updateStockProducts = () => {
-//     cart.forEach((product) => {
-//       const queryUpdate = doc(db, 'items', product.id);
-//       updateDoc(queryUpdate, {
-//         categoryId: product.categoryId,
-//         description: product.description,
-//         image: product.image,
-//         price: product.price,
-//         title: product.title,
-//         stock: product.stock - product.quantity,
-//       })
-//         .then(() => {
-//           if (cart[cart.length - 1].id === product.id) {
-//             clear();
-//             navigate('/');
-//           }
-//         })
-//         .catch(() => {
-//           console.log('error al actualizar stock');
-//         });
-//     });
-//   };
-
-//   const handleInputChange = (e) => {
-//     console.log(e.target);
-//     setOrder({
-//       ...order,
-//       buyer: {
-//         ...order.buyer,
-//         [e.target.name]: e.target.value,
-//       },
-//     });
-//   };
-
-//   return (
-//     <div style={{ display: 'flex', flexDirection: 'row' }}>
-//       {cart.length === 0 ? (
-//         <>
-//           <h2>No hay productos en tu carrito</h2>
-//           <Link to={'/'}>Volver a comprar</Link>
-//         </>
-//       ) : (
-//         <>
-//           <h1>Cart</h1>
-//           {cart.map((item) => (
-//             <div
-//               key={item.id}
-//               style={{
-//                 display: 'flex',
-//                 flexDirection: 'column',
-//                 alignItems: 'center',
-//               }}
-//             >
-//               <img
-//                 src={'../img/' + item.image}
-//                 alt={item.title}
-//                 width={'150px'}
-//               />
-//               <br />
-//               <h3>{item.title}</h3>
-//               <br />
-//               <p>${item.price} ------ </p>
-//               <br />
-//               <p>{item.quantity}</p>
-//               <button onClick={() => removeItem(item.id)}>
-//                 Eliminar producto
-//               </button>
-//             </div>
-//           ))}
-//         </>
-//       )}
-//       <div style={{ margin: '50px' }}>
-//         <div>
-//           <label>Nombre</label>
-//           <input
-//             name="name"
-//             type="text"
-//             value={order.buyer.name}
-//             onChange={handleInputChange}
-//           />
-//         </div>
-//         <br />
-//         <div>
-//           <label>Telefono</label>
-//           <input
-//             name="phone"
-//             type="number"
-//             value={order.buyer.phone}
-//             onChange={handleInputChange}
-//           />
-//         </div>
-//         <br />
-//         <div>
-//           <label>Correo</label>
-//           <input
-//             name="email"
-//             type="email"
-//             value={order.buyer.email}
-//             onChange={handleInputChange}
-//           />
-//         </div>
-//       </div>
-//       <div style={{ marginLeft: '100px', marginTop: '150px' }}>
-//         <button onClick={createOrder}>Crear orden</button>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Cart;
--- src/components/CheckOut/CheckOut.js
+++ src/components/CheckOut/CheckOut.js
@@ -11,7 +11,11 @@ const CheckOut = () => {
 
     const navigate = useNavigate();
 
-    const { cart, removeItem, clear, totalQuantity } = useContext(CartContext);
+    const { cart, clear, totalQuantity } = useContext(CartContext);
+
+    
+// -------  FIREBASE
+
 
     const [order, setOrder] = useState({
         buyer: {
@@ -41,7 +45,7 @@ const CheckOut = () => {
             .then(({ id }) => {
                 console.log(id)
                 updateStockProducts();
-                swal("Gracias por tu compra!", "", "success");
+                swal('Gracias por tu compra! Su ID es:', id, "success");
             })
             .catch(() =>
                 alert('Tu compra no fue realizada. Intenta mas tarde')
@@ -81,6 +85,10 @@ const CheckOut = () => {
         })
 
     }
+
+
+// -------  FIREBASE
+
 
 
     return (
@@ -158,153 +166,3 @@ const CheckOut = () => {
 }
 
 export default CheckOut;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import { useState, useContext } from "react";
-// import { CartContext } from "../../context/CartContext";
-// import { getFirestore, collection, addDoc } from "firebase/firestore";
-// import { Link } from "react-router-dom";
-// import moment from "moment/moment";
-
-
-
-// const CheckOut = () => {
-
-//     const { cart, clear } = useContext(CartContext);
-
-//     const [name, setName] = useState("");
-//     const [email, setEmail] = useState("");
-//     const [phone, setPhone] = useState(0);
-//     const [orderID, setOrderID] = useState();
-//     const [date, setDate] = useState("");
-//     const [total, setTotal] = useState(0);
-//     const [purchase, setPurchase] = useState([]);
-
-//     const managePurchase = (evt) => {
-//         evt.preventDefault();
-
-//         const db = getFirestore();
-
-//         let purchaseDate = new Date();
-//         setDate(purchaseDate.toLocaleString());
-
-//         const purchaseInfo = {
-//             buyer: {
-//                 name: name,
-//                 email: email,
-//                 phone: phone,
-//             },
-//             items: cart,
-//             total: cart.reduce(
-//                 (valorPasado, valorActual) =>
-//                     valorPasado + valorActual.price * valorActual.quantity,
-//                 0
-//             ),
-//             date: moment().format(),
-//         };
-
-//         const order = collection(db, 'orders');
-//         addDoc(purchaseInfo, order)
-//             .then(({ id }) => {
-//                 setOrderID(id);
-//             })
-//             .catch((error) => {
-//                 console.log(error);
-//             });
-//         updateStock(purchaseInfo);
-//     };
-
-//     const updateStock = (purchaseInfo) => {
-//         purchaseInfo.items.map((element) => {
-//             let id = element.item.id
-//             let quantity = element.quantity
-
-//             const db = getFirestore();
-//             const itemCollection = db.collection("productos");
-//             const itemFirestore = itemCollection.doc(id);
-//             itemFirestore.get().then((value) => {
-//                 let previousStock = value.data().stock
-//                 return itemFirestore.update({
-//                     stock: previousStock - quantity
-//                 });
-//             });
-//         });
-//     };
-
-
-
-//     return (
-//         <>
-//             {orderID ?
-//                 <div className="">
-//                     <h5>¡Muchas gracias por tu compra, {name}!</h5><br />
-//                     <p>
-//                         Tu número de orden es <span>{orderID}</span><br /><br />
-//                         Ya enviamos el detalle de tu compra y el código de seguimiento del envío a tu casilla de correo electrónico (<a id="email">{email}</a>).
-//                     </p>
-//                     <Link to={`/`} className="">Iniciar una nueva compra</Link>
-//                 </div>
-//                 :
-//                 <div className="">
-//                     <form className="" onSubmit={() => { managePurchase(); clear(); setTotal(); setPurchase(cart) }}>
-//                         <h5>Datos personales</h5>
-//                         <div className="">
-//                             <label htmlFor="name">Nombre</label>
-//                             <input type="text" className="" id="name" name="name" value={name} onChange={(evt) => { setName(evt.target.value) }} />
-//                         </div>
-                        
-//                         <div className="form-group my-4">
-//                             <label htmlFor="email">E-mail</label>
-//                             <input type="email" className="form-control" id="email" name="email" value={email} onChange={(evt) => { setEmail(evt.target.value) }} />
-//                         </div>
-                        
-//                         <div className="form-group my-4">
-//                             <label htmlFor="phone">Teléfono</label>
-//                             <input type="text" className="form-control" id="phone" name="phone" value={phone} onChange={(evt) => { setPhone(evt.target.value) }} />
-//                         </div>
-                        
-//                         <div className="checkoutButtonContainer">
-//                             <Link to={`/cart`}><button className="">Atrás</button></Link>
-//                             {email === name && phone ? <button type="submit" className="">Comprar</button> : <button type="submit" className="" /*disabled*/ >Comprar</button>}
-//                         </div>
-//                     </form>
-//                 </div>
-//             }
-//         </>
-//     );
-// };
-
-// export default CheckOut;
